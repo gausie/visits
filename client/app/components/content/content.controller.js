d93d4238802c7386081ef2cca4069da896623a87
Daniel Greane <dan@greane.co.uk>
2016-03-05 18:26:56 +0000

    [TASK] removed unneeded directive

--- client/app/components/content/content.controller.js
+++
@@ -1,5 +0,0 @@
-export default class ContentController {
-  constructor() {
-    this.name = 'content';
-  }
-}
--- client/app/components/content/content.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-import uiRouter from 'angular-ui-router';
-import contentComponent from './content.component';
-
-export default angular.module('content', [
-  uiRouter,
-])
-
-.component('content', contentComponent);
