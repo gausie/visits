acaab9c6deeaec21a42d7c5a7d73d80de561a0d2
Daniel Greane <dan@greane.co.uk>
2016-03-06 13:29:57 +0000

    Details and navbar config

--- client/app/common/navbar/navbar.controller.js
+++ client/app/common/navbar/navbar.controller.js
@@ -1,7 +1,14 @@
 class NavbarController {
-  constructor() {
+  constructor($window) {
     this.name = 'navbar';
+    this.type = this.type || 'default';
+    this.$window = $window;
+  }
+  back() {
+    this.$window.history.back();
   }
 }
+
+NavbarController.$inject = ['$window'];
 
 export default NavbarController;
